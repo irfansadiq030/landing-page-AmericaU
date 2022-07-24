59fa5edb4ee05355aa2199b907ff8ec493c7a4f6
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2022-07-24 13:26:02 +0100

    Sidebar Work On going
    Added Meet Heroes Image
    Added Portal Login Buttons
    Fixed Styling Issues.

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -10,6 +10,13 @@ const Sidebar = () => {
                     <img className='logo-img' src="images/logo.png" alt="" />
                 </div>
                 <Navmenu />
+                <div className="meet-heroes-img-container">
+                    <img className='meet-heroes-img' src="images/meethHeroes.jpg" alt="" />
+                </div>
+                <div className="portal-container">
+                    <p>Parent/Teacher Login</p>
+                </div>
+                <p className='logout-link'>Logout</p>
             </div>
         </>
     )
