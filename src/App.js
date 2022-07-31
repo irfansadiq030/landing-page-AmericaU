f2eeacafc0453b41e1a5f6a7e935ecd1c65fae07
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2022-07-31 13:46:34 +0500

    Fixed styling issues
    --Added Main content container background image.
    --WIN of mobile header.
    --Fixed styling issues of Sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Sidebar from './Components/Sidebar/Sidebar';
+import Main from './Components/Main/Main';
+import { useState } from 'react';
+// import MobileHeader from './Components/MobileHeader/MobileHeader';
 
 function App() {
   return (
     <>
-      <div className="components-container">
-        <Sidebar />
+      <div>
+        <div className="components-container">
+          <Sidebar />
+          <Main />
+        </div>
       </div>
     </>
   );
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Main.css'
+import './MobileHeader.css'
+
+const Main = (props) => {
+  
+  return (
+    <div className='main-container'>
+      <div className="mobile-header">
+        <div className="hamburger-container">
+          <i class="fa-solid fa-bars bars-icon"></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
