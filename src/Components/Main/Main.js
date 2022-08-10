c08d4ffbecad6461671ce0900a4853892f1d9d40
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2022-08-10 11:42:00 +0500

    Added Chat and Friends List features
    -- Added Chat input box, chat post button and chat outer frame.
    --Added Friends List box
    -- Added friends online/offline status indicator.
    --Added functionality on chat, friends and my heroes button Click.
    -- Fixed responsiveness bugs.

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './Main.css'
 import './MobileHeader.css'
 import BlogSlider from './BlogSlider/BlogSlider'
@@ -6,7 +6,7 @@ import { GlobalInfo } from '../../App';
 
 const Main = (props) => {
   const { sidebar, HideSidebar } = useContext(GlobalInfo);
-  // console.log(sidebar);
+  const [selectBox, setSelectBox] = useState(1);
 
   return (
     <div className='main-container'>
@@ -31,25 +31,80 @@ const Main = (props) => {
 
         <div className="character-container">
           <div className="power-select-character">
-            <img className='charcter-img' src="images/tbrender.png" alt="" />
-            <div className='dailyChalleng-box'>
-              <img className='box-img' src="images/DailyChallenge_box.png" alt="" />
-              <div className="name-container">
-                <p className='charcter-name'>Sir Itchay ninja</p>
-                <p className='charcter-name'>LV.2</p>
+            <div style={selectBox === 1 ? { display: 'flex', justifyContent: 'center', alignItems: 'center' } : { display: 'none' }} className=''>
+              <img className='charcter-img' src="images/tbrender.png" alt="" />
+              <div className='dailyChalleng-box'>
+                <img className='box-img' src="images/DailyChallenge_box.png" alt="" />
+                <div className="name-container">
+                  <p className='charcter-name'>Sir Itchay ninja</p>
+                  <p className='charcter-name'>LV.2</p>
+                </div>
               </div>
             </div>
+            <div style={selectBox === 2 ? { display: 'flex' } : { display: 'none' }} className='friends-tab'>
+              <input className='friends-name-search' type="text" placeholder='Enter name ...' />
+              <span className="login-to-heroes-text">Login to Heroes league to add friends</span>
+              <div className="friends-list-container">
+                <div className="friends-name-container">
+                  <span style={{ background: '#60cb3d' }} className="friends-status"></span>
+                  <span className="fr-name">29 CJohnson</span>
+                </div>
+                <div className="friends-name-container">
+                  <span style={{ background: '#60cb3d' }} className="friends-status"></span>
+                  <span className="fr-name">29 FMammeri</span>
+                </div>
+                <div className="friends-name-container">
+                  <span style={{ background: 'grey' }} className="friends-status"></span>
+                  <span className="fr-name">29 NSnodgrass</span>
+                </div>
+                <div className="friends-name-container">
+                  <span style={{ background: 'grey' }} className="friends-status"></span>
+                  <span className="fr-name">29 YSylla</span>
+                </div>
+                <div className="friends-name-container">
+                  <span style={{ background: 'grey' }} className="friends-status"></span>
+                  <span className="fr-name">28 irfansadiq</span>
+                </div>
+                {/* <div className="friends-name-container">29 FMammeri</div>
+                <div className="friends-name-container">29 NSnodgrass</div>
+                <div className="friends-name-container">29 YSylla</div>
+                <div className="friends-name-container">29 FMammeri</div> */}
+              </div>
+
+            </div>
+            <div style={selectBox === 3 ? { display: 'flex' } : { display: 'none' }} className='chat-tab'>
+              <div className="chat-txt-container">
+                {/* <div className="txt-msg">
+                  I'm ready to attack the pirates!
+                </div>
+                <span className='msg-time'>Charming Shyclown 9:35 am</span> */}
+              </div>
+              <form className="chat-category">
+                <select className='chat-dropdown'>
+                  <option value="actual value 1">Chat Category</option>
+                  <option value="actual value 2">Category A</option>
+                  <option value="actual value 3">Category B</option>
+                </select>
+                <input type="text" class='chat-txt' />
+                <button type="submit" className='post-btn'>
+                  <img className='btn-img' src="images/Portal_btnBlue.png" alt="" />
+                  <span className='post-btn-txt'>post</span>
+                </button>
+              </form>
+
+            </div>
+
           </div>
-          <div className="myheroes-container">
-            <img src="images/Portal_btnBlue.png" alt="" className="myheroes-img" />
+          <div onClick={() => setSelectBox(1)} style={selectBox === 1 ? { left: '20px', top: '150px', width: '120px', height: '37px' } : {}} className="myheroes-container">
+            <img alt='' src={selectBox === 1 ? 'images/Portal_btnBlue.png' : 'images/Portal_btnOrange.png'} className="myheroes-img" />
             <h1 className="myheroes-text">My Hero</h1>
           </div>
-          <div className="myfriends-container">
-            <img src="images/Portal_btnOrange.png" alt="" className="myheroes-img" />
+          <div onClick={() => setSelectBox(2)} style={selectBox === 2 ? { left: '20px', top: '193px', width: '120px', height: '37px' } : {}} className="myfriends-container">
+            <img src={selectBox === 2 ? 'images/Portal_btnBlue.png' : 'images/Portal_btnOrange.png'} alt="" className="myheroes-img" />
             <h1 className="myheroes-text">Friends</h1>
           </div>
-          <div className="chat-container">
-            <img src="images/Portal_btnOrange.png" alt="" className="myheroes-img" />
+          <div onClick={() => setSelectBox(3)} style={selectBox === 3 ? { left: '20px', top: '235px', width: '120px', height: '37px' } : {}} className="chat-container">
+            <img src={selectBox === 3 ? 'images/Portal_btnBlue.png' : 'images/Portal_btnOrange.png'} alt="" className="myheroes-img" />
             <h1 className="myheroes-text">Chat</h1>
           </div>
         </div>
