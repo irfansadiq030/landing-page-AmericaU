49edd98c7ea30f1c5cd9d2af87cfae2cd9346311
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2022-08-25 20:49:29 +0500

    Fixed Meet Heroes Page text click slide move
    Fixed videos page responsiveness.
    fixed meet heroes page text click and related slide appears.

--- src/Components/Main/MeetHeroes/MeetHeroes.js
+++ src/Components/Main/MeetHeroes/MeetHeroes.js
@@ -11,24 +11,48 @@ const heroesData = [
         youtubeVideoID: 'JuW25j47v-g'
     },
     {
-        img: 'DETECTIVE MR E-min.png',
-        youtubeVideoID: 'Z2lw8jjry3M'
+        img: 'Mental Ninja-min.png',
+        youtubeVideoID: 'E3BncLoUEKE'
+    },
+    {
+        img: 'Firewall-min.png',
+        youtubeVideoID: 'HHpwbTQXky0'
+    },
+    {
+        img: 'Gritster-min.png',
+        youtubeVideoID: 'vXQZ9um5Ugo'
+    },
+    {
+        img: 'Deep Blue-min.png',
+        youtubeVideoID: 'azBYgcvt8Wk'
+    },
+    {
+        img: 'Gold Angel-min.png',
+        youtubeVideoID: 'l6IIiEtmiR0'
     },
     {
         img: 'CHANCHITO-min.png',
         youtubeVideoID: 'rZh9RvNGQWQ'
     },
     {
-        img: 'IRON HAMMER AND THE YOUNG BUILDERS-min.png',
-        youtubeVideoID: 'P2xWdL0FiP4'
+        img: 'Arkseed.png',
+        youtubeVideoID: 'DWWmjrK6rvY'
     },
     {
-        img: 'DEEDLES-min.png',
-        youtubeVideoID: 'SC38A9LOaTc'
+        img: 'DENZILLION-min.png',
+        youtubeVideoID: 'YjAaxCsZnsc'
     },
     {
-        img: 'TRI-GUY-min.png',
-        youtubeVideoID: 'Q16NBGy5JBI'
+        img: 'Count Dinero-min.png',
+        youtubeVideoID: 'Sl9V96psMrY'
+    },
+    {
+        img: 'DETECTIVE MR E-min.png',
+        youtubeVideoID: 'Z2lw8jjry3M'
+    },
+    {
+        img: 'KID EINSTEIN-min.png',
+        youtubeVideoID: 'JuW25j47v-g'
     },
     {
         img: 'GENERAL WASHINGTON-min.png',
@@ -39,124 +63,108 @@ const heroesData = [
         youtubeVideoID: '5P9IJo34dQw'
     },
     {
-        img: 'DENZILLION-min.png',
-        youtubeVideoID: 'YjAaxCsZnsc'
+        img: 'TRI-GUY-min.png',
+        youtubeVideoID: 'Q16NBGy5JBI'
     },
     {
-        img: 'BULLSEYE BOT-min.jpg',
+        img: 'IRON HAMMER AND THE YOUNG BUILDERS-min.png',
+        youtubeVideoID: 'P2xWdL0FiP4'
+    },
+    {
+        img: 'RE-PETE-min.png',
+        youtubeVideoID: 'BpJ71LNZFhQ'
+    },
+    {
+        img: 'BULLSEYE BOT-min.png',
         youtubeVideoID: 'xp9rA3iX4A4'
+    },
+    {
+        img: 'KARMZAH-min.png',
+        youtubeVideoID: 'kxth_-IGV0I'
+    },
+    {
+        img: 'DEEDLES-min.png',
+        youtubeVideoID: 'SC38A9LOaTc'
     },
     {
         img: 'MASTER HEARME LONGWIND-min.png',
         youtubeVideoID: 'zvuGO9BfrMA'
     },
-    {
-        img: 'Arkseed.png',
-        youtubeVideoID: 'DWWmjrK6rvY'
-    },
-    {
-        img: 'Gold Angel-min.png',
-        youtubeVideoID: 'l6IIiEtmiR0'
-    },
-    {
-        img: 'Count Dinero-min.png',
-        youtubeVideoID: 'Sl9V96psMrY'
-    },
-    {
-        img: 'Deep Blue-min.png',
-        youtubeVideoID: 'azBYgcvt8Wk'
-    },
-    {
-        img: 'Gritster-min.png',
-        youtubeVideoID: 'vXQZ9um5Ugo'
-    },
-    {
-        img: 'Firewall-min.png',
-        youtubeVideoID: 'HHpwbTQXky0'
-    },
-    {
-        img: 'Mental Ninja-min.png',
-        youtubeVideoID: 'E3BncLoUEKE'
-    },
+
 ]
+// Heroes Names
+const heroesNames = [
+    'Dean Archer', 'Mental Ninja', 'Firewall', 'Gritster',
+    'Deep Blue', 'Gold Angel', 'Chanchito', 'Arskeed',
+    'Denzillion ', 'Count Dinero', 'Detective Mr. E', 'Kid Einstein', 'General Washington',
+    'Mighty Chondrian', 'Tri-Guy', 'Iron Hammer & The Young Builders', 'Re-Pete', 'Bullseye Bot',
+    'Karmzah ', 'Deedles ', 'Master Hearme Longwind'
+];
 const MeetHeroes = () => {
-    const [sliderRef, setSliderRef] = useState(null)
+    const [heroName, setHeroName] = useState(true);
+    const [sliderRef, setSliderRef] = useState(null);
+
+    // Onclick any hero name slide to display function
+    const slideGoto = (index) => {
+        sliderRef?.slickGoTo(index)
+    }
+
     const settings = {
         dots: false,
+        customPaging: function (i) {
+            return (
+                <span>{heroesNames[i]} </span>
+            );
+        },
+        // dotsClass: "slick-dots heroes-name-container",
         arrows: false,
         infinite: true,
+        lazyLoad:'ondemand',
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
         centerPadding: "20px",
         // responsive: [
         //     {
-        //         breakpoint: 480,
+        //         breakpoint: 600,
         //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1
+        //             dotsClass: "slick-dots fani mobile-heroNameContainer",
+        //             dots: true,
         //         }
         //     },
-        //     {
-        //         breakpoint: 1200,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 1,
-        //             initialSlide: 1
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 1440,
-        //         settings: {
-        //             slidesToShow: 3,
-        //             slidesToScroll: 1
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 2200,
-        //         settings: {
-        //             slidesToShow: 4,
-        //             slidesToScroll: 1
-        //         }
-        //     },
-
-
-
         // ]
+
     };
+
+
+
     return (
         <div className='meetHeroes-container'>
             <MobileHeader />
             <PageTitle title="Meet the Heroes" />
-            <img className='meetHeroes-main-img' src="images/vector.png" alt="" />
-            <div className="heroes-data-container">
-                {/* {
-                    heroesData.map((heroData, index) => {
+            <img onClick={() => setHeroName(!heroName)} className='meetHeroes-main-img' src="images/vector.png" alt="" />
+            <div className="heroName-container">
+                {
+                    heroesNames.map((name,index) => {
                         return (
-                            <div key={index}>
-                                <img loading="lazy" className='heroes-img-collection' src={`images/meet-heroes/${heroData.img}`} alt="" />
-                                <div className="heroes-video">
-                                    <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${heroData.youtubeVideoID}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                </div>
-                            </div>
+                            <span onClick={()=>slideGoto(index)} className="heroName"> {name}   &nbsp; &nbsp;| </span>
                         )
                     })
-                } */}
+                }
+
+                {/* <span className="heroName">Irfan | </span> */}
+            </div>
+            <div className="heroes-data-container">
                 <div className="left-arrow-container">
                     <img className='slider-arrow' onClick={sliderRef?.slickNext} src="images/icon-back.png" alt="" />
                 </div>
                 <Slider dots ref={setSliderRef} {...settings}>
-                    {/* <div className="meetHeroes-slide">
-                        <img loading="lazy" className='heroes-img-collection' src='images/meet-heroes/Firewall-min.png' alt="" />
-                        <div className="heroes-video">
-                            <iframe width="100%" height="100%" src='https://www.youtube.com/embed/Z2lw8jjry3M' title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                        </div>
-                    </div> */}
+
                     {
                         heroesData.map((heroData, index) => {
                             return (
                                 <div className="meetHeroes-slide" key={index}>
-                                    <img loading="lazy" className='heroes-img-collection' src={`images/meet-heroes/${heroData.img}`} alt="" />
+                                    <img className='heroes-img-collection' src={`images/meet-heroes/${heroData.img}`} alt="" />
                                     <div className="heroes-video">
                                         <iframe width="100%" height="100%" src={`https://www.youtube.com/embed/${heroData.youtubeVideoID}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                     </div>
