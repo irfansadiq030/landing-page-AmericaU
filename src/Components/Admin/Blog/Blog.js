27f03a5bb5757504935e64c6e2ad27d9a6633413
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2022-09-28 09:58:58 +0500

    Admin Main Page form created
    Created Form on admin main page.
    installed new package @heroicons/react.
    set page Layout for admin other pages.

--- src/Components/Admin/Blog/Blog.js
+++ src/Components/Admin/Blog/Blog.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Blog() {
     return (
-        <div>Blog</div>
+        <React.Fragment>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                <h1 className="text-2xl font-semibold text-gray-900">Blog Page</h1>
+            </div>
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+                {/* Replace with your content */}
+                <div className="py-4">
+                    <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                </div>
+                {/* /End replace */}
+            </div>
+        </React.Fragment>
     )
 }
