9c13bc10afc51b4eac15d343b5735803579a54e7
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2022-09-30 14:53:51 +0500

    Blog admin page setup
    Added form and blog listing table in admin blog table

--- src/Components/Admin/Blog/Blog.js
+++ src/Components/Admin/Blog/Blog.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react'
+import BlogTable from './BlogTable'
+import BlogForm from './BlogForm'
+
+
 
 export default function Blog() {
+    const [cmpSwitch, setCmpSwitch] = useState(true);
+    const toggleForm = () => {
+        setCmpSwitch(!cmpSwitch);
+    }
     return (
         <React.Fragment>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
@@ -9,7 +18,25 @@ export default function Blog() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 {/* Replace with your content */}
                 <div className="py-4">
-                    <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+                    {/* Form Area start */}
+                    <button
+                        onClick={toggleForm}
+                        type="button"
+                        className="mb-4 inline-flex items-center px-8 py-2.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        {cmpSwitch ? 'Add New' : 'Back'}
+                    </button>
+                    <div className='shadow sm:rounded-md sm:overflow-hidden pb-6'>
+                        {
+                            cmpSwitch ? <BlogTable /> : <BlogForm />
+                        }
+
+
+
+                        {/* Table START */}
+                        {/* <SeasonPassTable /> */}
+
+                    </div>
                 </div>
                 {/* /End replace */}
             </div>
