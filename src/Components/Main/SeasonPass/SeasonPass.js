4756cc9bda2c55fa36b22b5bfe03f38a032eb7f2
irfansadiq <32975448+irfansadiq030@users.noreply.github.com>
2022-11-16 16:32:56 +0500

    Season Pass Modal Created.
    Season Pass Modal Created.. work is still in progress

--- src/Components/Main/SeasonPass/SeasonPass.js
+++ src/Components/Main/SeasonPass/SeasonPass.js
@@ -5,9 +5,13 @@ import MobileHeader from '../MobileHeader/MobileHeader'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { getSeasons } from '../../../store/slices/season'
-import ModalImage from "react-modal-image";
+import { useState } from 'react'
+// import ModalImage from "react-modal-image";
+import { RiCloseCircleFill } from 'react-icons/ri'
+import Modal from 'react-modal';
 
 const SeasonPass = () => {
+    const [modalShow, setModalShow] = useState(false);
     const seasons = useSelector(getSeasons);
     console.log(seasons);
     return (
@@ -41,50 +45,59 @@ const SeasonPass = () => {
                     <img src="images/blog Detail.png" alt="" className="seasonPs-main-img" />
                     <div className="latestSeasonStories-container">
                         <div className="seasonBox1">
-                            <ModalImage
+                            {/* <ModalImage
                                 small={'images/Week 1 Story 2.png'}
                                 large={'images/blog Detail.png'}
                                 className="seasonItme-img"
-                            />
-                            {/* <img className='seasonItme-img' src="images/Week 1 Story 2.png" alt="" /> */}
+                            /> */}
+                            <img onClick={() => setModalShow(!modalShow)} className='seasonItme-img' src="images/Week 1 Story 2.png" alt="" />
                             <div className="latestSeasonPs-data">
                                 <h2 className="seasonPs-item-title">Slide Title</h2>
                                 <span className="seasonPs-item-date"> Mar,20,2022</span>
                             </div>
                         </div>
-                        
+
                         <div className="seasonBox1">
-                            <ModalImage
-                                small={'images/Week 1 Story 2.png'}
-                                large={'images/blog Detail.png'}
-                                className="seasonItme-img"
-                            />
-                            {/* <img className='seasonItme-img' src="images/Week 1 Story 2.png" alt="" /> */}
+
+                            <img className='seasonItme-img' src="images/Week 1 Story 2.png" alt="" />
                             <div className="latestSeasonPs-data">
                                 <h2 className="seasonPs-item-title">Slide Title</h2>
                                 <span className="seasonPs-item-date"> Mar,20,2022</span>
                             </div>
                         </div>
-                        
+
                         <div className="seasonBox1">
-                            <ModalImage
+                            {/* <ModalImage
                                 small={'images/Week 1 Story 2.png'}
                                 large={'images/blog Detail.png'}
                                 className="seasonItme-img"
-                            />
-                            {/* <img className='seasonItme-img' src="images/Week 1 Story 2.png" alt="" /> */}
+                            /> */}
+                            <img className='seasonItme-img' src="images/Week 1 Story 2.png" alt="" />
                             <div className="latestSeasonPs-data">
                                 <h2 className="seasonPs-item-title">Slide Title</h2>
                                 <span className="seasonPs-item-date"> Mar,20,2022</span>
                             </div>
                         </div>
-                        
+
                     </div>
                     <Link to="/seasonpass-list" className="readmore-seasonStories-btn">
                         Read More
                     </Link>
                 </div>
+                
+                {/* Modal */}
+                <div className={modalShow ? 'modal' :  `hide_modal`}>
+                    <div className="modal_container">
+                        <div className="modal-header">
+                            <span onClick={() => setModalShow(!modalShow)} className='modal_close_span'><RiCloseCircleFill color='#000' className='modalClose' /></span>
+                        </div>
+                        <div className="p-3 min-h-full min-w-full mx-auto flex justify-center">
+                            <img className='object-contain' src="images/blog Detail.png" alt="" />
+                        </div>
+                    </div> 
+                </div>
             </div>
+            
         </div>
     )
 }
