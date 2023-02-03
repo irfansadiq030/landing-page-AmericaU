eae0491466c7135197752ab8071d2330dffba3b3
Muhammad Yasir <shahzadyasir77@gmail.com>
2023-02-03 13:22:32 +0500

    Fixed logo path

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -24,7 +24,7 @@ const Sidebar = () => {
           <div className="logo-container">
             <Link onClick={() => HideSidebar(sidebar)} to="/">
               {" "}
-              <img className="logo-img" src={"images/logo.png"} alt="" />{" "}
+              <img className="logo-img" src={"logo.png"} alt="" />{" "}
             </Link>
           </div>
           <Navmenu />
